5562c05e564d23c3462dc58a11297d617eb8312b
Andrew Zaiets <andrew.zaec@gmail.com>
2021-08-31 17:48:58 +0300

    Initial Commit

---
+++ DogBreedsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogBreedsApp
+//
+//  Created by Andrew Zaiets on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
